0812c96c96ce295d74723231a18d7c64807a3262
Yuancheng-Xu <51889131+Yuancheng-Xu@users.noreply.github.com>
2019-07-23 17:39:40 -0700

    time series WGCNA update

---
+++ FF_timeSeries/Readme.txt
@@ -0,0 +1,7 @@
+WGCNA_TS_Documentation is description (LaTex) plus code and some step by step implementation.
+
+WGNA_TS.r is the code that you will actually run (only contains functions), 
+which will import SimData.r for generating CS/AR/... data and TimeSeriesSimilarity.r which will compute the 
+similarity between time series. 
+
+You have to install the package "rucrdtw" to use fastDTW. Please see the folder "Useful packages" for more details.
--- FF_timeSeries/TimeSeriesSimilarity.r
+++ FF_timeSeries/TimeSeriesSimilarity.r
@@ -1,6 +1,6 @@
-
-library("TSdist")
 library("proxy")
+library("dtw")
+library("rucrdtw")
 
 # Normalize each time series (not a column)
 normalize_TS = function(X,len_time){
@@ -39,18 +39,53 @@ similarity_TS = function(X,len_time,type){
            
         return (cor_mat)
         } # end "cor"
-
+    
+     ## type = fastDTW, no need to normalize ##
+    if (type == "fastDTW"){
+        # pre-allocate memory
+        dist_mat = matrix(0,p,p)
+        if (n==1){
+            for (i in 1:(p-1)){
+                for (j in (i+1):p){
+                    dist_mat[i,j] = ucrdtw_vv(X[,i], X[,j], 0.05)$distance
+                }
+            }
+        }else{
+            flag = 1 # the first time index of the current sample
+            for (sample in 1:n){
+                for (i in 1:(p-1)){
+                    for (j in (i+1):p){
+                        dist_mat[i,j] = (dist_mat[i,j]
+                        +(ucrdtw_vv(X[flag:(flag+len_time[sample]-1),i], 
+                                                  X[flag:(flag+len_time[sample]-1),j],
+                                                  0.05)$distance)*len_time[sample])
+                    }
+                }                
+                flag = flag+len_time[sample]
+            }
+            dist_mat = dist_mat/(sum(len_time))
+        }
+        # complete the matrix (diag=0 automatically)
+        dist_mat[lower.tri(dist_mat)] = t(dist_mat)[lower.tri(dist_mat)]
+        
+        # transform dist to similarity
+        sim_matrix = 1/(1+dist_mat)
+        
+        return (sim_matrix)
+        } # end "fastDTW"
+    
+    #### data Z score normaliztion #####
     X = normalize_TS(X,len_time) # normalize time series
     #check for missing value (may due to sigma=0)
     tmp = sum(is.na(X))
     if(tmp>0){
         stop(sprintf("There are %d missing values in normalized X",tmp))
     }
+    ### end normalization ###
     
-    # compute similarity pairwisely with different measure
     
     # "L2" using dist(M) which computes dist between rows of M
-    if (type == "L2"){
+    if (type == "L2"|type == "Euclidean"){
         # first compute distance matrix
         if (n==1){
             dist_obj = dist(t(X),method = "euclidean")
@@ -76,53 +111,34 @@ similarity_TS = function(X,len_time,type){
         return (sim_matrix)
         } # end "L2"
     
-    ## type = cor ##
-    if (type == "cor"){
+    # "dtw" using dtwDist(M) which computes dist between rows of M
+    if (type == "dtw" | type == "DTW"){
         # first compute distance matrix
         if (n==1){
-            cor_mat = cor(X)
+            dist_obj = dtwDist(t(X))
         }else{
-            # cor_mat can be computed like a vector! but I have to create it
-            cor_mat = cor(X[1:len_time[1],])*len_time[1]
-            flag = 1+len_time[1] # the first time index of the current sample
-            for (sample in 2:n){
-                cor_mat = (cor_mat + 
-            cor(X[flag:(flag+len_time[sample]-1),])*len_time[sample])
-                
-                flag = flag+len_time[sample]
-            }
-            cor_mat = cor_mat/(sum(len_time))
-        }     
-           
-        return (cor_mat)
-        } # end "cor"
-    
-    # Other method: use proxy(dist function) and TSdist package
-    # Note: They are really slow!
-    if (n==1){
-            dist_obj = dist(t(X),method="tsDistances",distance=type)
-        }else{
-            # dist_obj can be computed like a vector! but I have to create it            
-            dist_obj = dist(t(X[1:len_time[1],]),
-                            method="tsDistances",distance=type)*len_time[1]
+            # dist_obj can be computed like a vector! but I have to create it
+            dist_obj = dtwDist(t(X[1:len_time[1],]))*len_time[1]
             flag = 1+len_time[1] # the first time index of the current sample
             for (sample in 2:n){
                 dist_obj = (dist_obj + 
-            dist(t(X[flag:(flag+len_time[sample]-1),]),
-                   method="tsDistances",distance=type)*len_time[sample])
+            dtwDist(t(X[flag:(flag+len_time[sample]-1),]))*len_time[sample])
                 
                 flag = flag+len_time[sample]
             }
             dist_obj = dist_obj/(sum(len_time))
-        }
+        }     
     
-    # transform dist to similarity
-    sim_obj = 1/(1+dist_obj)
-    # similarity matrix
-    sim_matrix = as.matrix(sim_obj)
-    diag(sim_matrix) = 1
+        # transform dist to similarity
+        sim_obj = 1/(1+dist_obj)
+        # similarity matrix
+        sim_matrix = as.matrix(sim_obj)
+        diag(sim_matrix) = 1
         
-    return (sim_matrix)
+        return (sim_matrix)
+        } # end "dtw"
     
+    
+    stop("The type is not availble")
     
     }
